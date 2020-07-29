a01cb4acec3a7e0e5f85975a09f43a839ca045c8
Anthony Osiol <funnynoun@gmail.com>
2020-07-29 12:47:00 -0400

    changed facebook and instagram icons and applied links, new npm package

--- client/src/components/FrontPage/FrontPage.js
+++ client/src/components/FrontPage/FrontPage.js
@@ -9,7 +9,7 @@ function FrontPage() {
       <div className="contact-tag">
         <p>6010 HERMITAGE ROAD RICHMOND, VIRGINIA </p>
         <p> (804) 264-7400</p>
-        <p>EAT@HOBNOBRVA.COM</p>
+        {/* <p>EAT@HOBNOBRVA.COM</p> */}
       </div>
       <br></br>
       <br></br>
